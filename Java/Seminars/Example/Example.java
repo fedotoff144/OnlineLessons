1af4eaba2699eef6e192b870ea4923b9b8aded75
fedotoff144 <fedotoff144@gmail.com>
2023-04-17 23:08:02 +0300

    some changes

---
+++ Java/Seminars/Example/Example.java
@@ -0,0 +1,17 @@
+package Example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Example {
+    public static void main(String[] args) {
+//        List<String> list = new ArrayList<>(Arrays.asList("000","111","222"));
+//        list.set(0, "tree");
+//        System.out.println(list);
+        System.out.println("\n<<<<<< Автомат игрушек >>>>>>\nКоманды для выбора: ADD_TOYS, WIN_TOY, VIEW_TOYS, HELP, EXIT");
+        System.out.println("-------------------------------------------------------------");
+//        System.out.println("ADD_TOYS - добавить игрушку в автомат\nWIN_TOY - разыграть игрушку\nVIEW_TOYS - посмотреть все игрушки в автомате\nEXIT - выход");
+
+    }
+}
