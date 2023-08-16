a4f7ed6271a88030ad67c1a736ec37d1f63ff36a
fedotoff144 <fedotoff144@gmail.com>
2023-08-16 16:59:18 +0300

    finished tasks for lesson 2 python

--- Dive into python/Seminars/Lesson_02/Task_06.py
+++ Dive into python/Seminars/Lesson_02/Task_06.py
@@ -17,42 +17,41 @@ MAX_COMMISSION = 600
 COMMISSION_FOR_RICH = 0.1  # 10%
 LIMIT_FOR_RICH = 5_000_000
 CONTROL_VALUE = 50
-BONUS_STEP = 3 # каждая третья операция со счетом
+BONUS_STEP = 3  # каждая третья операция со счетом
 BONUS_OPERATIONS = 0.03  # бонусные проценты за каждую третью операцию 3%
 all_money = 0
 operations_counter = 1
 
 
-def get_money_request(message: str) -> int:
+def validation_request(message: str) -> int:
     while True:
         try:
-            cash = int(input(message))
+            input_number: int = int(input(message))
             break
         except ValueError:
             print("Некорректный ввод. Пожалуйста, введите число.")
         except Exception as e:
             print("Произошла ошибка:", str(e))
-    return cash
+    return input_number
 
 
-def check_money_request(request: int) -> bool:
-    res = True
-    if request % CONTROL_VALUE != 0:
-        res = False
-    return res
+def check_control_value(request_money: int) -> bool:
+    if request_money % CONTROL_VALUE == 0:
+        return True
+    else:
+        print('Сумма должна быть кратна 50')
+        return False
 
 
 def calculation_commission(request_money: int) -> int | float:
     if all_money >= LIMIT_FOR_RICH:
         return request_money * COMMISSION_FOR_RICH
     else:
-
         result = request_money * COMMISSION
-
         if result < MIN_COMMISSION:
             return MIN_COMMISSION
         elif result > MAX_COMMISSION:
-            return MIN_COMMISSION
+            return MAX_COMMISSION
         return result
 
 
@@ -63,26 +62,50 @@ def counter_bonus(request_money: int) -> int | float:
         return 0
 
 
+def output(money: int | float, commission: int | float, bonus: int | float) -> str:
+    if bonus != 0:
+        print(f'Комиссия за операцию: -{commission} | бонусы за операцию: +{bonus} | на вашем счету:{money}')
+    else:
+        print(f'Комиссия за операцию: -{commission} | на вашем счету:{money}')
+
+
 print('Добро пожаловать в банкомат!\n1. Пополнить баланс\n2. Снять наличные\n0. Выход')
 while True:
-    choice = int(input('Введите команду: '))
+    choice = validation_request('Введите команду: ')
     match choice:
         case 1:
-            request_money = get_money_request('Введите сумму пополнения баланса: ')
-            if check_money_request(request_money):
-                all_money += request_money + counter_bonus(request_money)
-                print('На вашем счету:' + str(all_money))
-                operations_counter += 1
-            else:
-                print('Сумма должна быть кратна 50')
+            print('На вашем счету:' + str(all_money))
+
+            while True:
+                request_money = validation_request('Введите сумму пополнения баланса: ')
+                if check_control_value(request_money):
+                    commission = calculation_commission(request_money)
+                    bonus = counter_bonus(request_money)
+                    all_money += request_money - commission + bonus
+
+                    output(all_money, commission, bonus)
+                    operations_counter += 1
+                    break
 
         case 2:
-            request_money = get_money_request('Введите сумму снятия со счета: ')
-            print(all_money)
+            print('На вашем счету:' + str(all_money))
+
+            while True:
+                request_money = validation_request('Введите сумму снятия со счета: ')
+                if check_control_value(request_money):
+                    commission = calculation_commission(request_money)
+                    bonus = counter_bonus(request_money)
+                    if request_money + calculation_commission(request_money) > all_money:
+                        print('На счету недостаточно средств!')
+                    else:
+                        all_money -= request_money + commission - bonus
+                        output(all_money, commission, bonus)
+                        operations_counter += 1
+                    break
 
         case 0:
             print('До свидания!')
             exit()
-        case _:
-            print('Ошибка! Повторите ввод.')
 
+        case _:
+            print('Команда меню не найдена, повторите ввод!')
